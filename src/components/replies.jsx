e1c8cdca5c49feffd2103a6a136cb10b99c887f7
Banja David Lotigo <banja@kali.lan>
2023-02-28 14:55:02 -0500

    Reply Component - Add text area to write a comment to a topic - Add a submit button to reply a topic

--- src/components/replies.jsx
+++ src/components/replies.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Replies = () => {
+    
+    const [reply, setReply] = useState("");
+
+    const handleChange= (e) => {
+        setReply(prev => ({
+            ...prev, [e.target.name]: e.target.value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ reply });
+    }
     return (
-        <div>
-            This is Replies
-        </div>
+        <main
+            style={{
+                //backgroundColor: "#ccc",
+                display: "flex", 
+                flexDirection: "column", 
+                justifyContent: "center",
+                alignItems: "center",
+                //width: "300px"
+            }}
+        >
+            <h3>Comment </h3>
+            <form onSubmit={ handleSubmit }>
+                    <div style={{ display: "flex", flexDirection: "column"}}>
+                        <label htmlFor="reply">Comment to this topic</label>
+                        <textarea 
+                            rows={5}
+                            type="text"
+                            name="reply"
+                            id="reply"
+                            required
+                            value={ reply }
+                            onChange={ handleChange }
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="SUBMIT COMMENT" 
+                        />
+                    </div>
+            </form>
+        </main>
     );
 }
 
