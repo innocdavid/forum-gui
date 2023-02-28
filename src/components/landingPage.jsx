e1a3de1386983f108fe24078082789a2e935420e
Banja David Lotigo <banja@kali.lan>
2023-02-28 10:09:44 -0500

    Landing Page Component - Add a landing page component

---
+++ src/components/landingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+    return (
+        <main style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h1>Welcome to talkForm</h1>
+        </main>
+    )
+}
+
+export default LandingPage;
