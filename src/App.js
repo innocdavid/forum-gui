740ed662cd1bba9722802c024f6cb16c8ddcc805
Banja David Lotigo <banja@kali.lan>
2023-02-28 08:44:36 -0500

    React Router Package - Add react-router-dom - Add / route for the login component - Add /register route for the register component - Add /dashboard route for the home component - Add :id/replies route for the replies component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './components/register';
+import Login from './components/login';
+import Home from './components/home';
+import Replies from './components/replies';
 
 import './App.css';
+
 
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
+          <Route path="/dashboard" element={ <Home /> } /> 
+          <Route path="/:id/replies" element={ <Replies /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
