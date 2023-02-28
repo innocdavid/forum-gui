1b99a8c2fc7acc30eadf5b46b34c2feff2e7928b
Banja David Lotigo <banja@kali.lan>
2023-02-28 08:33:18 -0500

    Initial Project Commit - Add login component - Add register component - Add home component - Add replies component - Add header component

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            This is Homme
+        </>
+    );
+}
+
+export default Home;
---
+++ src/components/replies.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Replies = () => {
+    return (
+        <div>
+            This is Replies
+        </div>
+    );
+}
+
+export default Replies;
