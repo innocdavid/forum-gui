67e22a1691730c6bb14b958259c0b99ea996e69f
Banja David Lotigo <banja@kali.lan>
2023-02-28 14:40:05 -0500

    Home Component - Add an input field for topic title - Add an input field for topic description - Add a create new topic button

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React,{ useState } from "react";
 
 const Home = () => {
+
+    const [thread, setThread] = useState({
+        title: "",
+        description: ""
+    });
+
+    const handleChange = (e) => {
+        setThread(prev => ({
+            ...prev, [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log()
+    }
     return (
-        <>
-            This is Homme
-        </>
+        <main
+            style={{
+                //backgroundColor: "#ccc",
+                display: "flex", 
+                flexDirection: "column", 
+                justifyContent: "center",
+                alignItems: "center",
+                //width: "300px"
+            }}
+        >
+            <h2>Create a new topic</h2>
+                <form onSubmit={ handleSubmit }>
+                    <div style={{ display: "flex", flexDirection: "column"}}>
+                        <label htmlFor="title">Title</label>
+                        <input 
+                            type="text"
+                            name="title"
+                            id="title"
+                            required
+                            value={ thread.title }
+                            onChange={ handleChange }
+                        />
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column"}}>
+                        <label htmlFor="description">Password</label>
+                        <textarea 
+                            type="text"
+                            name="description"
+                            id="description"
+                            required
+                            value={ thread.description }
+                            onChange={ handleChange }
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="CREATE NEW TOPIC" 
+                        />
+                </div>
+            </form>
+        </main>
     );
 }
 
