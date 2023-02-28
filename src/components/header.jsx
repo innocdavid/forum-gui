e3b357524cf3c72922fef32d21d847a4f9242946
Banja David Lotigo <banja@kali.lan>
2023-02-28 09:52:38 -0500

    Header Component - Add nav section for the header component - Add application name - Add a logout button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
 const Header = () => {
+    const signOut = () => {
+        alert("User signed out!");
+    }
     return (
-        <div>
-            This is a header
-        </div>
+        <nav 
+            style={{ 
+                padding: "5px",
+                backgroundColor: "#ccc",
+                justifyContent: "space-between",
+                display: "flex", 
+            }}
+        >
+            <h2>talkForum</h2>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <button onClick={ signOut }>Sign out</button>
+            </div>
+            
+        </nav>
     );
 }
 
