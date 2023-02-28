dad06d330ffc080278724bcb02fe4aee69273776
Banja David Lotigo <banja@kali.lan>
2023-02-28 09:52:24 -0500

    Update: Add header and main section to the app component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/home';
 import Replies from './components/replies';
 
 import './App.css';
+import Header from './components/header';
 
 
 
@@ -13,12 +14,17 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Login /> } />
-          <Route path="/register" element={ <Register /> } />
-          <Route path="/dashboard" element={ <Home /> } /> 
-          <Route path="/:id/replies" element={ <Replies /> } />
-        </Routes>
+        <header>
+          < Header/>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={ <Login /> } />
+            <Route path="/register" element={ <Register /> } />
+            <Route path="/dashboard" element={ <Home /> } /> 
+            <Route path="/:id/replies" element={ <Replies /> } />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
